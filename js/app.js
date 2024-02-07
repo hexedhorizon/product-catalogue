b441f78feb927d6ee3a25309e712b73a6b96b858
Cliffmeister <dev.clfm5t12@gmail.com>
2024-02-07 21:49:06 +0800

    Added card styling on products

--- js/app.js
+++ js/app.js
@@ -9,10 +9,16 @@ fetch('data/data.json')
       data.forEach(product => {
         const listItem = document.createElement('p');
         listItem.innerHTML = `
-          <div>${product.product_name}</div>
-          <div>${product.product_price}</div>
-          <div>${product.product_description}</div>
-          <div>Added on: ${product.product_date_added}</div>
+
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${product.product_name} - ${product.product_price} Php</h5>
+              <p class="card-text">${product.product_description}</p>
+              <p class="card-text">${product.product_date_added}</p>
+              
+              <a href="#" class="btn btn-primary">Add to Cart</a>
+            </div>
         `;
         productListContainer.appendChild(listItem);
       }); 
